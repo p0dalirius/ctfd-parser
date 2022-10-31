bf02da21785b09b265da9a2e8dfc56d2c01ee9df
Igor Kuzmenkov <74809945+IgorDuino@users.noreply.github.com>
2022-10-31 15:55:06 +0300

    Add interactive password input (#8)
    * feat: interactive password input, fix: handle login error
    
    * refactor: remake format strings, autopep8

--- ctfd_parser.py
+++ ctfd_parser.py
@@ -10,6 +10,7 @@ import requests
 import re
 import os
 from concurrent.futures import ThreadPoolExecutor
+from getpass import getpass
 
 
 def os_filename_sanitize(s):
@@ -21,8 +22,6 @@ def os_filename_sanitize(s):
 
 
 class CTFdParser(object):
-    """docstring for CTFdParser."""
-
     def __init__(self, target, login, password, basedir="Challenges"):
         super(CTFdParser, self).__init__()
         self.target = target
@@ -36,7 +35,8 @@ class CTFdParser(object):
 
     def login(self):
         r = self.session.get(self.target + '/login')
-        matched = re.search(b"""('csrfNonce':[ \t]+"([a-f0-9A-F]+))""", r.content)
+        matched = re.search(
+            b"""('csrfNonce':[ \t]+"([a-f0-9A-F]+))""", r.content)
         nonce = ""
         if matched is not None:
             nonce = matched.groups()[1]
@@ -49,13 +49,10 @@ class CTFdParser(object):
                 'nonce': nonce.decode('UTF-8')
             }
         )
-        if r.status_code == 200:
-            return True
-        else:
-            return False
+
+        return 'Your username or password is incorrect' not in r.text
 
     def get_challenges(self, threads=8):
-        """Documentation for get_challenges"""
         r = self.session.get(self.target + "/api/v1/challenges")
 
         if r.status_code == 200:
@@ -65,22 +62,27 @@ class CTFdParser(object):
                     self.challenges = json_challs['data']
                     self._parse(threads=threads)
                 else:
-                    print("[warn] An error occurred while requesting /api/v1/challenges")
+                    print(
+                        "[warn] An error occurred while requesting /api/v1/challenges")
             return json_challs
         else:
             return None
 
     def _parse(self, threads=8):
         # Categories
-        self.categories = sorted(list(set([chall["category"] for chall in self.challenges])))
+        self.categories = sorted(
+            list(set([chall["category"] for chall in self.challenges])))
 
-        print('\x1b[1m[\x1b[93m+\x1b[0m\x1b[1m]\x1b[0m Found %d categories !' % len(self.categories))
+        print(
+            f'\x1b[1m[\x1b[93m+\x1b[0m\x1b[1m]\x1b[0m Found {len(self.categories)} categories !')
 
         # Parsing challenges
         for category in self.categories:
-            print("\x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m Parsing challenges of category : \x1b[95m%s\x1b[0m" % category)
+            print(
+                f"\x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m Parsing challenges of category : \x1b[95m{category}\x1b[0m")
 
-            challs_of_category = [c for c in self.challenges if c['category'] == category]
+            challs_of_category = [
+                c for c in self.challenges if c['category'] == category]
 
             # Waits for all the threads to be completed
             with ThreadPoolExecutor(max_workers=min(threads, len(challs_of_category))) as tp:
@@ -90,41 +92,45 @@ class CTFdParser(object):
 
     def dump_challenge(self, category, challenge):
         if challenge["solved_by_me"]:
-            print("   \x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m \x1b[1;92m✅\x1b[0m \x1b[96m%s\x1b[0m" % challenge["name"])
+            print(
+                f"   \x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m \x1b[1;92m✅\x1b[0m \x1b[96m{challenge['name']}\x1b[0m")
         else:
-            print("   \x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m \x1b[1;91m❌\x1b[0m \x1b[96m%s\x1b[0m" % challenge["name"])
+            print(
+                f"   \x1b[1m[\x1b[93m>\x1b[0m\x1b[1m]\x1b[0m \x1b[1;91m❌\x1b[0m \x1b[96m{challenge['name']}\x1b[0m")
 
-        folder = os.path.sep.join([self.basedir, os_filename_sanitize(category), os_filename_sanitize(challenge["name"])])
+        folder = os.path.sep.join([self.basedir, os_filename_sanitize(
+            category), os_filename_sanitize(challenge["name"])])
         if not os.path.exists(folder):
             os.makedirs(folder)
 
         # Readme.md
         f = open(folder + os.path.sep + "README.md", 'w')
-        f.write("# %s\n\n" % challenge["name"])
-        f.write("**Category** : %s\n" % challenge["category"])
-        f.write("**Points** : %s\n\n" % challenge["value"])
+        f.write(f"# {challenge['name']}\n\n")
+        f.write(f"**Category** : {challenge['category']}\n")
+        f.write(f"**Points** : {challenge['value']}\n\n")
 
         chall_json = self.get_challenge_by_id(challenge["id"])["data"]
-        f.write("%s\n\n" % chall_json["description"])
+        f.write(f"{chall_json['description']}\n\n")
 
         connection_info = chall_json["connection_info"]
         if connection_info is not None:
             if len(connection_info) != 0:
-                f.write("%s\n\n" % connection_info)
+                f.write(f"{connection_info}\n\n")
 
         # Get challenge files
         if len(chall_json["files"]) != 0:
             f.write("## Files : \n")
             for file_url in chall_json["files"]:
-                r = self.session.head(self.target + file_url, allow_redirects=True)
+                r = self.session.head(
+                    self.target + file_url, allow_redirects=True)
                 size = int(r.headers["Content-Length"])
                 if "?" in file_url:
                     filename = os.path.basename(file_url.split('?')[0])
                 else:
                     filename = os.path.basename(file_url)
-                #
-                f.write(" - [%s](./%s)\n" % (filename, filename))
-                if size < (50 * 1024 * 1024):  # 50 Mo
+
+                f.write(f" - [{filename}](./{filename})\n")
+                if size < (50 * 1024 * 1024):  # 50 Mb
                     r = self.session.get(self.target + file_url, stream=True)
                     with open(folder + os.path.sep + filename, "wb") as fdl:
                         for chunk in r.iter_content(chunk_size=16 * 1024):
@@ -139,7 +145,7 @@ class CTFdParser(object):
 
     def get_challenge_by_id(self, chall_id: int):
         """Documentation for get_challenge_by_id"""
-        r = self.session.get(self.target + '/api/v1/challenges/%d' % chall_id)
+        r = self.session.get(self.target + f'/api/v1/challenges/{chall_id}')
         json_chall = None
         if r.status_code == 200:
             json_chall = json.loads(r.content)
@@ -160,12 +166,18 @@ def header():
 def parseArgs():
     header()
     parser = argparse.ArgumentParser(description="CTFdParser")
-    parser.add_argument("-t", "--target", required=True, help="CTFd target (domain or ip)")
-    parser.add_argument("-o", "--output", required=False, help="Output directory")
-    parser.add_argument("-u", "--user", required=True, help="Username to login to CTFd")
-    parser.add_argument("-p", "--password", required=True, help="Password to login to CTFd")
-    parser.add_argument("-T", "--threads", required=False, default=8, type=int, help="Number of threads (default: 8)")
-    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Verbose mode. (default: False)")
+    parser.add_argument("-t", "--target", required=True,
+                        help="CTFd target (domain or ip)")
+    parser.add_argument("-o", "--output", required=False,
+                        help="Output directory")
+    parser.add_argument("-u", "--user", required=True,
+                        help="Username to login to CTFd")
+    parser.add_argument("-p", "--password", required=False,
+                        help="Password to login to CTFd (default: interactive)")
+    parser.add_argument("-T", "--threads", required=False,
+                        default=8, type=int, help="Number of threads (default: 8)")
+    parser.add_argument("-v", "--verbose", default=False,
+                        action="store_true", help="Verbose mode. (default: False)")
     return parser.parse_args()
 
 
@@ -177,11 +189,16 @@ if __name__ == '__main__':
     args.target = args.target.rstrip('/')
 
     if args.verbose:
-        print("[>] Target URL: %s" % args.target)
-    
+        print(f"[>] Target URL: {args.target}")
+
     if args.output is None:
-        args.output = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Challenges")
+        args.output = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "Challenges")
+    if args.password is None:
+        args.password = getpass("Password: ")
 
     cp = CTFdParser(args.target, args.user, args.password, args.output)
-    cp.login()
-    cp.get_challenges(threads=args.threads)
+    if cp.login():
+        cp.get_challenges(threads=args.threads)
+    else:
+        print("[-] Login failed")
